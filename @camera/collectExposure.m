38c7b62bcfd03ab1c5ff6bb4e7a59aa7ae0abdb6
PolishookDavid <45033848+PolishookDavid@users.noreply.github.com>
2020-03-16 22:44:44 +0200

    Delete collectExposure.m

--- @camera/collectExposure.m
+++
@@ -1,8 +0,0 @@
-function img=collectExposure(CameraObj)
-   % collect the exposed frame, but only if an exposure was started!
-   img = CameraObj.CameraDriverHndl.collectExposure;
-   switch CameraObj.CameraDriverHndl.lastError
-       case "no image to read because exposure not started"
-           CameraObj.lastError = "no image to read because exposure did not start";
-   end
-end
