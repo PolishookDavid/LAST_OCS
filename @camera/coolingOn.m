0dcf8d0fa2672bb81b04149c7f87a6ff1ae9b43a
PolishookDavid <45033848+PolishookDavid@users.noreply.github.com>
2020-03-16 22:44:37 +0200

    Delete coolingOn.m

--- @camera/coolingOn.m
+++
@@ -1,5 +0,0 @@
-function coolingOn(CameraObj,temp)
-% Turn cooling on and set target temperature, if given
-% the default target tempetarure, if not given, is -20°C (arbitrarily)
-   CameraObj.CameraDriverHndl.coolingOn(temp);
-end
