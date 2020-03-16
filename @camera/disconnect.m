41833ca49dcb8f41d62a0c0b033f6da8a65701db
PolishookDavid <45033848+PolishookDavid@users.noreply.github.com>
2020-03-16 22:45:36 +0200

    Delete disconnect.m

--- @camera/disconnect.m
+++
@@ -1,8 +0,0 @@
-function success=disconnect(CameraObj)
-    % Close the connection with the camera registered in the current camera object
-   success=CameraObj.CameraDriverHndl.disconnect;
-   switch CameraObj.CameraDriverHndl.lastError
-       case "could not disconnect camera"
-           CameraObj.lastError = "could not disconnect camera";
-   end
-end
