467a8d8fdae23ed1e77aed68a141f3a033654d59
Enrico Segre (last04) <enrico.segre@weizmann.ac.il>
2021-08-19 09:25:51 +0000

    remove unused camera.isIdle

--- +obs/@camera/isIdle.m
+++
@@ -1,9 +0,0 @@
-function Flag = isIdle(CameraObj)
-    % Return true (per camera) if camera is idle
-    switch lower(CameraObj.CamStatus)
-        case 'idle'
-            Flag = true;
-        otherwise
-            Flag = false;
-    end
-end
