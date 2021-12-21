de510eef93061e4263f5a0e6ff441fc111a2b3c0
EranOfek <ofek.eran@gmail.com>
2021-12-21 14:30:23 +0200

    adding plate alignment info in alignCameraRotation

--- +obs/+util/+align/alignCameraRotation.m
+++ +obs/+util/+align/alignCameraRotation.m
@@ -1,5 +1,5 @@
 function Summary = alignCameraRotation(UnitCS, Args)
-    % An aux function for camera rotation alignment
+    % An aux function for camera rotation alignment and telescope alignment
     %       Take images along several HA on the Equator. For each image,
     %       solve astrometry and calculate field rotation.
     %       Return the median rotation for each camera.
@@ -19,10 +19,21 @@ function Summary = alignCameraRotation(UnitCS, Args)
     % Outout : - A summary structure
     %            .S - Structure array of [HA, Cam] for astrometric summary
     %                   for each HA/camera.
+    %            .MountJ - J2000.0 RA/Dec/HA for each mount pointing.
+    %            .MountD - Equinox of date mount (no distortion) coo for
+    %                   each mount pointing.
     %            .MedRot - A vector of median rotation, for each camera
     %                   [deg].
     %            .StdRot - A vector of std rotation, for each camera
     %                   [deg].
+    %            .OffsetLong - Matrix of offset between mount J2000 RA position
+    %                   and camera position for each [Ha, Cam]. [deg]
+    %            .OffsetLat - Matrix of offset between mount J2000 Dec position
+    %                   and camera position for each [Ha, Cam]. [deg]
+    %            .OffsetDist - Matrix of dist between mount J2000 position
+    %                   and camera position for each [Ha, Cam]. [deg]
+    %            .OffsetPA - Matrix of PA between mount J2000 position
+    %                   and camera position for each [Ha, Cam]. [deg]
     % Author : Eran Ofek (Dec 2021)
     % Example: Summary = obs.util.align.alignCameraRotation(UnitCS);
     
@@ -37,6 +48,8 @@ function Summary = alignCameraRotation(UnitCS, Args)
         
     end
    
+    RAD = 180./pi;
+    
     if isempty(Args.TelOffsets)
         %% read telescope offset relative to mount from properties
     end
@@ -57,6 +70,15 @@ function Summary = alignCameraRotation(UnitCS, Args)
         RA  = OutCoo(1);
         Dec = OutCoo(2);
         
+        MountJ(Iha).RA  = RA;
+        MountJ(Iha).Dec = Dec;
+        MountJ(Iha).HA  = OutCoo(3);
+        
+        MountD(Iha).RA  = UnitCS.Mount.RA;
+        MountD(Iha).Dec = UnitCS.Mount.Dec;
+        MountD(Iha).HA  = UnitCS.Mount.HA;
+        
+        
         % take exposure and wait till finish
         UnitCS.takeExposure(Args.Cameras, Args.ExpTime, 1);
         UnitCS.readyToExpose(Args.Cameras, true, Args.ExpTime+10);
@@ -64,18 +86,37 @@ function Summary = alignCameraRotation(UnitCS, Args)
         % get image names
         % Image full names are stored in a cell array FileNames{1..4}
         for Icam=1:1:Ncam
+            IndCam = Args.Cameras(Icam);
             try
-                FileNames{Icam} = UnitCS.Camera{Icam}.classCommand('LastImageName');
-                [~, ~, S(Iha,Icam)] = imProc.astrometry.astrometryCropped(FileNames{Icam}, 'RA',RA+Args.TelOffsets(Icam,1),...
-                                                                                       'Dec',Dec+Args.TelOffsets(Icam,2));
+                FileNames{Icam} = UnitCS.Camera{IndCam}.classCommand('LastImageName');
+                [~, ~, S(Iha,Icam)] = imProc.astrometry.astrometryCropped(FileNames{IndCam}, 'RA',RA+Args.TelOffsets(IndCam,1),...
+                                                                                       'Dec',Dec+Args.TelOffsets(IndCam,2));
             end
         end
     end
         
     Summary.S = S;
+    Summary.MountJ = MountJ;
+    Summary.MountD = MountD;
+    
     for Icam=1:1:Ncam
         Summary.MedRot(Icam)  = median([S(:,Icam).Rotation]);
         Summary.StdRot(Icam)  = std([S(:,Icam).Rotation]);
     end
+    
+    Summary.OffsetLong = nan(Nha, Ncam);
+    Summary.OffsetLat  = nan(Nha, Ncam);
+    Summary.OffsetDist = nan(Nha, Ncam);
+    Summary.OffsetPA   = nan(Nha, Ncam);
+    for Iha=1:1:Nha
+        for Icam=1:1:Ncam
+            IndCam = Args.Cameras(Icam);  
+            [Summary.OffsetLong(Iha,Icam), Summary.OffsetLat(Iha,Icam), Summary.OffsetDist(Iha,Icam), Summary.OffsetPA(Iha,Icam)] = celestial.coo.sphere_offset(Summary.MountJ(Iha).RA./RAD, Summary.S(Iha,Icam)./RAD);
+        end
+    end
+    Summary.OffsetLong = Summary.OffsetLong.*RAD;
+    Summary.OffsetLat  = Summary.OffsetLat .*RAD;
+    Summary.OffsetDist = Summary.OffsetDist.*RAD;
+    Summary.OffsetPA   = Summary.OffsetPA  .*RAD;
     
 end
