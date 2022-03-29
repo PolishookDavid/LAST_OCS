d0a95078e4999beaeb6ad2dcedaa756fa60ee97d
Enrico Segre on last02w <enrico.segre@weizmann.ac.il>
2022-03-29 15:54:10 +0000

    fixing bug in takeDarks

--- +obs/@unitCS/takeDarks.m
+++ +obs/@unitCS/takeDarks.m
@@ -1,4 +1,4 @@
-function takeDarks(Unit,Cameras,ExpTime,Nimages,varargin)
+function takeDarks(Unit,Cameras,ExpTime,Nimages,Args)
 % Take a sequence of dark images
 %
 % Same as takeExposure
@@ -7,29 +7,37 @@ function takeDarks(Unit,Cameras,ExpTime,Nimages,varargin)
 % Assign deafult values if arguments are not given
 % (No, not giving the arguments will not set to takeExposure's deafults)
 
-if ~exist('Cameras','var') || isempty(Cameras)
-    Cameras=1:numel(Unit.Camera);
+arguments
+    Unit
+    Cameras = [];
+    ExpTime = 20;
+    Nimages = 20;
+    Args.takeExposureArgs cell   = {};
 end
-
-if ~exist('ExpTime','var') || isempty(ExpTime)
-    ExpTime=zeros(size(Cameras));
-    for i=1:numel(Cameras)
-        ExpTime(i) = Unit.Camera{Cameras(i)}.classCommand('ExpTime');
-    end
-else
-    if numel(ExpTime)==1
-        ExpTime=repmat(ExpTime,size(Cameras));
-    end
-    for i=1:numel(Cameras)
-        Unit.Camera{Cameras(i)}.classCommand('ExpTime=%f;',ExpTime(i));
-    end
-end
-
-if ~exist('Nimages','var') || isempty(Nimages)
-    Nimages = 10;
-end
+% 
+% if ~exist('Cameras','var') || isempty(Cameras)
+%     Cameras=1:numel(Unit.Camera);
+% end
+% 
+% if ~exist('ExpTime','var') || isempty(ExpTime)
+%     ExpTime=zeros(size(Cameras));
+%     for i=1:numel(Cameras)
+%         ExpTime(i) = Unit.Camera{Cameras(i)}.classCommand('ExpTime');
+%     end
+% else
+%     if numel(ExpTime)==1
+%         ExpTime=repmat(ExpTime,size(Cameras));
+%     end
+%     for i=1:numel(Cameras)
+%         Unit.Camera{Cameras(i)}.classCommand('ExpTime=%f;',ExpTime(i));
+%     end
+% end
+% 
+% if ~exist('Nimages','var') || isempty(Nimages)
+%     Nimages = 10;
+% end
 
 % end of Mathia's code
     
-Unit.takeExposure(Cameras, ExpTime, Nimages, varargin{:}, 'ImType','dark');
+Unit.takeExposure(Cameras, ExpTime, Nimages, Args.takeExposureArgs{:}, 'ImType','dark');
 
