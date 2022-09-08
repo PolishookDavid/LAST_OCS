cc411729fda2f8e30998983711b26f29d51fc003
Enrico Segre <enrico.segre@weizmann.ac.il>
2022-09-08 11:28:00 +0300

    revise image keys in camera FITS header

--- +obs/@camera/imageHeader.m
+++ +obs/@camera/imageHeader.m
@@ -60,12 +60,6 @@ function [HeaderCell,Info]=imageHeader(CameraObj)
     I = I + 1;
     Info(I).Name = 'JD';
     Info(I).Val  = 1721058.5 + CameraObj.classCommand('TimeStartLastImage');
-    
-    
-    % internal gain
-    I = I + 1;
-    Info(I).Name = 'INTGAIN';
-    Info(I).Val  = CameraObj.classCommand('Gain');
 
     Keys={'GAIN','DARKCUR','READNOI','SATURVAL','NONLIN'};
     % get the camera Config structure once
@@ -103,84 +97,29 @@ function [HeaderCell,Info]=imageHeader(CameraObj)
     Info(I).Val  = CameraObj.classCommand('CameraName');
 
     I = I + 1;
-    Info(I).Name = 'SENSTEMP';
+    Info(I).Name = 'CAMTEMP';
     Info(I).Val  = CameraObj.classCommand('Temperature');
 
+    I = I + 1;
+    Info(I).Name = 'CAMCOOL';
+    Info(I).Val  = CameraObj.classCommand('CoolingPower');
+
+    I = I + 1;
+    Info(I).Name = 'CAMMODE';
+    Info(I).Val  = CameraObj.classCommand('ReadMode');
+
+    I = I + 1;
+    Info(I).Name = 'CAMGAIN';
+    Info(I).Val  = CameraObj.classCommand('Gain');
+
+    I = I + 1;
+    Info(I).Name = 'CAMOFFS';
+    Info(I).Val  = CameraObj.classCommand('Offset');
 
     % build header from structure
     N = numel(Info);
     HeaderCell = cell(N,3);
     HeaderCell(:,1) = {Info.Name};
     HeaderCell(:,2) = {Info.Val};
-
-    %%% OLD - REMOVE after testing
-    
-%     Info.NAXIS    = numel(SizeImIJ);
-%     
-%     Info.NAXIS1   = SizeImIJ(2);
-%     Info.NAXIS2   = SizeImIJ(1);
-%     Info.BITPIX   = -32;
-%     Info.BZERO    = 0.0;
-%     Info.BSCALE   = 1.0;
-%     Info.IMTYPE   = CameraObj.classCommand('ImType');
-%     Info.OBJECT   = CameraObj.classCommand('Object');
-% 
-%     % internal gain
-%     Info.INTGAIN  = CameraObj.classCommand('Gain');
-% 
-%     % keys which may be or be not in Config:
-%     % Gain, Read noise, Dark current
-%     Keys={'GAIN','DARKCUR','READNOI'};
-%     % get the camera Config structure once
-%     CameraConfig = CameraObj.classCommand('Config');
-%     for i=1:numel(Keys)
-%         Field = Keys{i};
-%         %if isfield(CameraObj.classCommand('Config'),Field)
-%         if isfield(CameraConfig, Field)
-%             Info.(Field)     = CameraConfig.(Field);  %CameraObj.classCommand('Config').(Field);
-%         else
-%             Info.(Field)     = NaN;
-%         end
-%     end
-%     %
-%     Info.BINX     = CameraObj.classCommand('Binning(1)');
-%     Info.BINY     = CameraObj.classCommand('Binning(2)');
-%     % 
-%     Info.CamNum   = CameraObj.classCommand('CameraNumber');
-%     Info.CamPos   = CameraObj.classCommand('CameraPos');
-%     %Info.CamType  = class(CameraObj); % redundent
-%     Info.CamModel = CameraObj.classCommand('CameraModel');
-%     Info.CamName  = CameraObj.classCommand('CameraName');
-% 
-%     Info.JD       = 1721058.5 + CameraObj.classCommand('TimeStartLastImage');
-%     %Info.ExpTime  = CameraObj.classCommand('LastImageExpTime');
-%     Info.ExpTime  = CameraObj.classCommand('ExpTime');
-%     
-%     % added by Enrico
-%     Info.SENSTEMP = CameraObj.classCommand('Temperature');
-
-    % eventual additional comment fields written independently in
-    %  ConfigHeader:
-    % ConfigHeader may contain additional constant keyword/values taht will
-    % be written to all headers.
-%    CommentsDB = CameraObj.classCommand('ConfigHeader');
-
-%     FN  = fieldnames(Info);
-%     Nfn = numel(FN);
-%     if ~isempty(CommentsDB)
-%         CommentFN = fieldnames(CommentsDB);
-%     end
-%     HeaderCell = cell(Nfn,3);
-%     for Ifn=1:Nfn
-%         HeaderCell{Ifn,1} = upper(FN{Ifn});
-%         HeaderCell{Ifn,2} = Info.(FN{Ifn});
-%         if ~isempty(CommentsDB)
-%             % get comment
-%             Ind = find(strcmpi(FN{Ifn},CommentFN));
-%             if ~isempty(Ind)
-%                 HeaderCell{Ifn,3} = CommentsDB.(CommentFN{Ind});
-%             end
-%         end
-%     end
 
 end
