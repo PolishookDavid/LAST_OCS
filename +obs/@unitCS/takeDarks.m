fff3d7111800416eb3994b43f4a768c87fe54cda
Enrico Segre <enrico.segre@weizmann.ac.il>
2022-09-15 09:46:17 +0300

    try takeDarks with case for single short exposures

--- +obs/@unitCS/takeDarks.m
+++ +obs/@unitCS/takeDarks.m
@@ -14,30 +14,16 @@ arguments
     Nimages = 20;
     Args.takeExposureArgs cell   = {};
 end
-% 
-% if ~exist('Cameras','var') || isempty(Cameras)
-%     Cameras=1:numel(Unit.Camera);
-% end
-% 
-% if ~exist('ExpTime','var') || isempty(ExpTime)
-%     ExpTime=zeros(size(Cameras));
-%     for i=1:numel(Cameras)
-%         ExpTime(i) = Unit.Camera{Cameras(i)}.classCommand('ExpTime');
-%     end
-% else
-%     if numel(ExpTime)==1
-%         ExpTime=repmat(ExpTime,size(Cameras));
-%     end
-%     for i=1:numel(Cameras)
-%         Unit.Camera{Cameras(i)}.classCommand('ExpTime=%f;',ExpTime(i));
-%     end
-% end
-% 
-% if ~exist('Nimages','var') || isempty(Nimages)
-%     Nimages = 10;
-% end
 
-% end of Mathia's code
+if ExpTime>5 || Nimages==1
+    Unit.takeExposure(Cameras, ExpTime, Nimages, Args.takeExposureArgs{:},...
+                      'ImType','dark', 'Object',[]);
+else
+    % loop on single exposures, to be sure that they can be saved
+    for i=1:Nimages
+        Unit.takeExposure(Cameras, ExpTime, 1, Args.takeExposureArgs{:},...
+                          'ImType','dark', 'Object',[]);
+        Unit.readyToExpose(Cameras,'Wait',true,'Timeout',ExpTime+7);
+    end
+end
     
-Unit.takeExposure(Cameras, ExpTime, Nimages, Args.takeExposureArgs{:}, 'ImType','dark', 'Object',[]);
-
