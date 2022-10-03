c1666b556e0405cecf3970594329f73f76d6f526
Enrico Segre <enrico.segre@weizmann.ac.il>
2022-10-03 17:37:05 +0300

    provisional function to urn on/off slave debugging for the whole unit

---
+++ testing/debugging.m
@@ -0,0 +1,18 @@
+function debugging(P,tof)
+% taylored for turning debugging logs on or off with the present
+%  structure of 4 slaves across two computers
+    if ~isempty([P.Slave{1}.PID,P.Slave{2}.PID,P.Slave{3}.PID,P.Slave{4}.PID])
+        fprintf('the debugging status can only be changed when slaves are disconnected!\n')
+        return
+    end
+    for i=1:4
+        P.Slave{i}.Logging=tof;
+        P.Slave{i}.LoggingDir=fullfile('/',P.Slave{i}.Host,'data','Logs');
+    end
+    P.connect
+    for i=1:4
+        P.Camera{i}.DebugOutput=tof;
+        P.Camera{i}.DebugLogLevel=5;
+    end
+
+    
