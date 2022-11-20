9ac42379b63025919caf30a83bf1c996bfa1ced6
Eran Ofek <ofek.eran@gmail.com>
2022-11-20 09:35:14 +0000

    changed loopOverTargets

--- +obs/+util/+observation/loopOverTargets.m
+++ +obs/+util/+observation/loopOverTargets.m
@@ -14,7 +14,7 @@ function loopOverTargets(Unit, Args)
         Args.Nimages = 20;
         Args.NLoops  = 1;     %
         Args.CoordFileName  = '/home/ocs/target_coordinates.txt';
-        Args.MinAlt   = 30; % [deg] not implemented
+        Args.MinAlt   = 30; % [deg]
         Args.ObsCoo   = [35.0407331, 30.0529838] % right order? [LONG, LAT]
     end
     
@@ -29,57 +29,60 @@ function loopOverTargets(Unit, Args)
 
     % reading target coordinates from file with format name,ra,dec
     [name, RA, Dec] = textread(Args.CoordFileName, '%s %f %f', 'delimiter',',');
+    Ntargets = length(RA);
     
     Nloops = Args.NLoops;
-    fprintf('%i fields in target list.\n\n',length(RA))
+    fprintf('%i fields in target list.\n\n',Ntargets)
     
     for Iloop=1:1:Nloops
 
-        % check which of the targets are observable
-        JD = celestial.time.julday;
-        [Flag,FlagRes] = celestial.coo.is_coordinate_ok(RA./RAD, Dec./RAD, JD, ...
-                'Lon', Unit.Mount.ObsLon./RAD, ...
-                'Lat', Unit.Mount.ObsLat./RAD, ...
-                'AltMinConst', Unit.Mount.MinAlt./RAD,...
-                'AzAltConst', Unit.Mount.AzAltLimit./RAD);
-        
-        name_observable = name(Flag);
-        RA_observable = RA(Flag);
-        Dec_observable = Dec(Flag);
-        Ntarget = sum(Flag);
-        fprintf('%d out of %d fields are observable.\n',Ntarget,length(RA))
-        fprintf('Starting loop %i.\n\n',Iloop)
+        fprintf('Starting loop %i out of %i.\n\n',Iloop,Nloops)
 
         % get observations for all targets
-        for Itarget=1:1:Ntarget
+        for Itarget=1:1:Ntargets
+            
+            % check if the target is observable
+            JD = celestial.time.julday;
+            [Flag,FlagRes] = celestial.coo.is_coordinate_ok(RA(Itarget)./RAD, Dec(Itarget)./RAD, JD, ...
+                    'Lon', Unit.Mount.ObsLon./RAD, ...
+                    'Lat', Unit.Mount.ObsLat./RAD, ...
+                    'AltMinConst', Args.MinAlt./RAD,...
+                    'AzAltConst', Unit.Mount.AzAltLimit./RAD);
         
-            fprintf('Observing field %d out of %d - Name=%s, RA=%.2f, Dec=%.2f\n',Itarget,Ntarget,name_observable{Itarget},RA_observable(Itarget), Dec_observable(Itarget));
-            if exist('~/abort','file')>0
-                delete('~/abort');
-                error('user abort file found');
-            end
-            
-            Unit.Mount.goToTarget(RA_observable(Itarget), Dec_observable(Itarget));
-            Unit.Mount.waitFinish;
-            pause(2);
-            
-            fprintf('Actual pointing: RA=%f, Dec=%f\n',Unit.Mount.RA, Unit.Mount.Dec);
-            
-            if ~Unit.readyToExpose('Wait',true, 'Timeout',Timeout)
-                fprintf('Cameras not ready after timeout - abort.\n\n')
-                break;
-            end    
+            if ~Flag
+                fprintf('Field %d is not observable.\n\n',Itarget)
+            else
 
-            if ~isempty(Args.ExpTime)
-                Unit.takeExposure([],Args.ExpTime,Args.Nimages);
-                fprintf('Waiting for exposures to finish\n\n');
-            end
+                if exist('~/abort','file')>0
+                    delete('~/abort');
+                    error('user abort file found');
+                end
+                
+                
+                fprintf('Observing field %d out of %d - Name=%s, RA=%.2f, Dec=%.2f\n',Itarget,Ntargets,name{Itarget},RA(Itarget), Dec(Itarget));
+
+                Unit.Mount.goToTarget(RA(Itarget), Dec(Itarget));
+                Unit.Mount.waitFinish;
+                pause(2);
+            
+                fprintf('Actual pointing: RA=%f, Dec=%f\n',Unit.Mount.RA, Unit.Mount.Dec);
+            
+                if ~Unit.readyToExpose('Wait',true, 'Timeout',Timeout)
+                    fprintf('Cameras not ready after timeout - abort.\n\n')
+                    break;
+                end    
+
+                if ~isempty(Args.ExpTime)
+                    Unit.takeExposure([],Args.ExpTime,Args.Nimages);
+                    fprintf('Waiting for exposures to finish\n\n');
+                end
         
-            pause(Args.ExpTime*(Args.Nimages+1)+4);
+                pause(Args.ExpTime*(Args.Nimages+1)+4);
 
-            if ~Unit.readyToExpose('Wait',true, 'Timeout',Timeout)
-                fprintf('Cameras not ready after timeout - abort.\n\n')
-                break;
+                if ~Unit.readyToExpose('Wait',true, 'Timeout',Timeout)
+                    fprintf('Cameras not ready after timeout - abort.\n\n')
+                    break;
+                end
             end    
         end
     end
