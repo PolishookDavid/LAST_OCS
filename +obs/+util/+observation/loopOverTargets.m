21be0a1b2ba956f5ae1f42a24fe587227295bbab
EranOfek <ofek.eran@gmail.com>
2023-03-23 16:35:18 +0000

    added focusing by temp and logging to obs script

--- +obs/+util/+observation/loopOverTargets.m
+++ +obs/+util/+observation/loopOverTargets.m
@@ -20,17 +20,27 @@ function loopOverTargets(Unit, Args)
     
     RAD = 180./pi;
     
-    %if isempty(Args.ExpTime)
-    %    Timeout = 60;
-    %else
-    %    Timeout = Args.ExpTime+60;
-    %end
     Timeout=60;
+    MountNumberStr = string(Unit.MountNumber);
+
+    % TODO: pass log dir as an argument and create dir if not present
+    logFileName = '~/log/log_loopOverTargets_M'+MountNumberStr+'_'+datestr(now,'YYYY-mm-DD')+'.txt','a+';
+                    
+    % columns of logfile
+    if ~isfile(logFileName)
+        logFile = fopen(logFileName,'a+');
+        fprintf(logFile,'datetime, '...
+            +'targetname, '...
+            +'RA, '...
+            +'Dec, '...
+            +'ExpTime, '...
+            +'NImages\n');
+        fclose(logFile);
+    end
+
 
     % reading target coordinates from file with format name,ra,dec
     [name, RA, Dec] = textread(Args.CoordFileName, '%s %f %f', 'delimiter',',');
-    RA
-    Dec
     Ntargets = length(RA);
     
     Nloops = Args.NLoops;
@@ -64,6 +74,9 @@ function loopOverTargets(Unit, Args)
                 fprintf('Observing field %d out of %d - Name=%s, RA=%.2f, Dec=%.2f\n',Itarget,Ntargets,name{Itarget},RA(Itarget), Dec(Itarget));
 
                 Unit.Mount.goToTarget(RA(Itarget), Dec(Itarget));
+                for IFocuser=[1,2,3,4]
+                    Unit.Slave{IFocuser}.Messenger.send(['P.focusByTemperature(' num2str(IFocuser) ')']); 
+                end
                 Unit.Mount.waitFinish;
                 pause(2);
             
@@ -75,8 +88,20 @@ function loopOverTargets(Unit, Args)
                 end    
 
                 if ~isempty(Args.ExpTime)
+                    % logging
+                    logFile = fopen(logFileName,'a+');
+                    fprintf(logFile,string(datestr(now))+', '...
+                        +name(Itarget)+', '...
+                        +string(Unit.Mount.RA)+', '...
+                        +string(Unit.Mount.Dec)+', '...
+                        +string(Args.ExpTime)+', '...
+                        +string(Args.Nimages)+'\n');
+                    fclose(logFile);
+
+  
                     Unit.takeExposure([],Args.ExpTime,Args.Nimages);
                     fprintf('Waiting for exposures to finish\n\n');
+                    
                 end
         
                 pause(Args.ExpTime*(Args.Nimages+1)+4);
