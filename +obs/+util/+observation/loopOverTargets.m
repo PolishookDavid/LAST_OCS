ebf7848b5012bf496ab2d45228c4cbc3cfed66d4
noralinn <nora.linn.strotjohann@gmail.com>
2023-04-01 17:59:00 +0000

    archived loopOverTargets and replaced it by loopOverTargets2 which uses the Target class

--- +obs/+util/+observation/loopOverTargets.m
+++ +obs/+util/+observation/loopOverTargets.m
@@ -1,30 +1,40 @@
 function loopOverTargets(Unit, Args)
-    % Loop over list of targets and observe them
-    % program checks by itself if targets are observable
-    % Ra,Dec of targets are provided in file. Default is ~/target_coordinates.txt
-    % The best way to interrupt the observations is creating the file ~/abort
+    % reads in target list from file Args.CoordFileName
+    % waits if no target observable
+    % otherwise observe them in the provided order
+    % records all obtained observations in log file
     %
-    % Example: obs.util.observation.loopOverTargets(P, 'ExpTime',5,'Nimages',1, 'NLoops',1)
+    % The best way to interrupt the observations is creating the file
+    % ~/abort_obs
     %
-    % written by Nora Nov. 2022, based on pointing model script
+    % touch ~/abort_and_shutdown will interrupt the observations and
+    % shutdown the unit
+    %
+    % Example: obs.util.observation.loopOverTargets(Unit,'NLoops',1,'CoordFileName','/home/ocs/target_coordinates.txt')
+    %
+    % written by Nora March 2023, based on pointing model script
    
     arguments
         Unit        
-        Args.ExpTime  = 20;     % if empty - only move without exposing
-        Args.Nimages = 20;
+        %Args.ExpTime  = 20;     % default values given by
+        %celestial.Targets.createList
+        Args.NperVisit = 20;
         Args.NLoops  = 1;     %
-        Args.CoordFileName  = '/home/ocs/target_coordinates_sne.txt';
+        Args.CoordFileName  = '/home/ocs/target_coordinates.txt';
         Args.MinAlt   = 30; % [deg]
-        Args.ObsCoo   = [35.0407331, 30.0529838] % [LONG, LAT]
+        Args.ObsCoo   = [35.0407331, 30.0529838]; % [LONG, LAT]
+        %Args.DeltaJD  = 0 % fraction of the day added to JD to trick mount into thinking it's night
     end
     
     RAD = 180./pi;
     
     Timeout=60;
     MountNumberStr = string(Unit.MountNumber);
+    dt = datetime('now')-hours(6); % ensure that entire night is in same logfile
+    datestring = datestr(dt, 'YYYY-mm-DD');
 
     % TODO: pass log dir as an argument and create dir if not present
-    logFileName = '~/log/log_loopOverTargets_M'+MountNumberStr+'_'+datestr(now,'YYYY-mm-DD')+'.txt','a+';
+    logFileName = '~/log/log_loopOverTargets_M'+MountNumberStr+'_'+datestring+'.txt';
                     
     % columns of logfile
     if ~isfile(logFileName)
@@ -35,42 +45,76 @@ function loopOverTargets(Unit, Args)
 
 
     % reading target coordinates from file with format name,ra,dec
-    [name, RA, Dec] = textread(Args.CoordFileName, '%s %f %f', 'delimiter',',');
-    Ntargets = length(RA);
+    [name, RAStr, DecStr] = textread(Args.CoordFileName, '%s %s %s', 'delimiter',',');
     
+    Ntargets = length(RAStr);
+    RA = zeros(Ntargets, 1);
+    Dec = zeros(Ntargets, 1);
+    for Itarget=1:1:Ntargets,
+        RA(Itarget) = str2double(RAStr{Itarget});
+        Dec(Itarget) = str2double(DecStr{Itarget});
+        if isnan(RA(Itarget))
+            [RATemp, DecTemp, ~]=celestial.coo.convert2equatorial(RAStr{Itarget},DecStr{Itarget});
+            RA(Itarget) = RATemp;
+            Dec(Itarget) = DecTemp;
+        end
+    end
+    T=celestial.Targets.createList('RA',RA,'Dec',Dec,'TargetName',name);
+    T.Data.NperVisit = ones(Ntargets,1)*Args.NperVisit;
+    fprintf('Number of images per visit: %i\n', Args.NperVisit);
+
+   
     Nloops = Args.NLoops;
     fprintf('%i fields in target list.\n\n',Ntargets)
     
+    
+    
     for Iloop=1:1:Nloops
+        
+        JD = celestial.time.julday; % + Args.DeltaJD;
+        [FlagAll, Flag] = isVisible(T, JD);
+        fprintf('%i targets are observable.\n', sum(FlagAll))
 
+        while sum(FlagAll)==0
+            pause(120)
+            JD = celestial.time.julday; % + Args.DeltaJD;
+            [FlagAll, Flag] = isVisible(T, JD);
+            fprintf('%i targets are observable. Waiting 2 minutes.\n', sum(FlagAll))
+        end
+        
         fprintf('Starting loop %i out of %i.\n\n',Iloop,Nloops)
 
         % get observations for all targets
         for Itarget=1:1:Ntargets
             
+            if exist('~/abort_obs','file')>0
+                delete('~/abort_obs');
+                error('user abort_obs file found');
+            end
+            
+            if exist('~/abort_and_shutdown','file')>0
+                delete('~/abort_and_shutdown');
+                Unit.shutdown
+                pause(60)
+                error('user abort_and_shutdown file found');
+            end
+
+            
             % check if the target is observable
-            JD = celestial.time.julday;
-            [Flag,FlagRes] = celestial.coo.is_coordinate_ok(RA(Itarget)./RAD, Dec(Itarget)./RAD, JD, ...
-                    'Lon', Unit.Mount.ObsLon./RAD, ...
-                    'Lat', Unit.Mount.ObsLat./RAD, ...
-                    'AltMinConst', Args.MinAlt./RAD,...
-                    'AzAltConst', Unit.Mount.AzAltLimit./RAD);
-        
-            if ~Flag
+            JD = celestial.time.julday; % + Args.DeltaJD;
+            [FlagAll, Flag] = isVisible(T, JD);
+                    
+            if ~FlagAll(Itarget)
                 fprintf('Field %d is not observable.\n\n',Itarget)
             else
-
-                if exist('~/abort','file')>0
-                    delete('~/abort');
-                    error('user abort file found');
-                end
                 
                 
-                fprintf('Observing field %d out of %d - Name=%s, RA=%.2f, Dec=%.2f\n',Itarget,Ntargets,name{Itarget},RA(Itarget), Dec(Itarget));
+                fprintf('Observing field %d out of %d - Name=%s, RA=%.2f, Dec=%.2f\n',...
+                    Itarget,Ntargets,T.TargetName{Itarget},T.RA(Itarget), T.Dec(Itarget));
 
-                Unit.Mount.goToTarget(RA(Itarget), Dec(Itarget));
+                Unit.Mount.goToTarget(T.RA(Itarget), T.Dec(Itarget));
                 for IFocuser=[1,2,3,4]
-                    % TODO 'Unit' should not be hard coded
+                    % TODO: 'Unit' should not be hard coded
                     Unit.Slave{IFocuser}.Messenger.send(['Unit.focusByTemperature(' num2str(IFocuser) ')']); 
                 end
                 Unit.Mount.waitFinish;
@@ -78,30 +122,27 @@ function loopOverTargets(Unit, Args)
             
                 fprintf('Actual pointing: RA=%f, Dec=%f\n',Unit.Mount.RA, Unit.Mount.Dec);
                 fprintf('Altitude: %f\n', Unit.Mount.Alt);
-                
+
                 if ~Unit.readyToExpose('Wait',true, 'Timeout',Timeout)
                     fprintf('Cameras not ready after timeout - abort.\n\n')
                     break;
                 end    
 
-                if ~isempty(Args.ExpTime)
-                    % logging
-                    logFile = fopen(logFileName,'a+');
-                    fprintf(logFile,string(datestr(now))+', '...
-                        +name(Itarget)+', '...
-                        +string(Unit.Mount.RA)+', '...
-                        +string(Unit.Mount.Dec)+', '...
-                        +string(Args.ExpTime)+', '...
-                        +string(Args.Nimages)+'\n');
-                    fclose(logFile);
+                % logging
+                logFile = fopen(logFileName,'a+');
+                fprintf(logFile,string(datestr(now, 'YYYYmmDD.HHMMSS'))+', '...
+                    +T.TargetName{Itarget}+', '...
+                    +string(Unit.Mount.RA)+', '...
+                    +string(Unit.Mount.Dec)+', '...
+                    +string(T.ExpTime(Itarget))+', '...
+                    +string(T.NperVisit(Itarget))+'\n');
+                fclose(logFile);
 
   
-                    Unit.takeExposure([],Args.ExpTime,Args.Nimages);
-                    fprintf('Waiting for exposures to finish\n\n');
-                    
-                end
-        
-                pause(Args.ExpTime*(Args.Nimages+1)+4);
+                Unit.takeExposure([],T.ExpTime(Itarget),T.NperVisit(Itarget));
+                fprintf('Waiting for exposures to finish\n\n');
+                
+                pause(T.ExpTime(Itarget)*(T.NperVisit(Itarget)+1)+4);
 
                 if ~Unit.readyToExpose('Wait',true, 'Timeout',Timeout)
                     fprintf('Cameras not ready after timeout - abort.\n\n')
