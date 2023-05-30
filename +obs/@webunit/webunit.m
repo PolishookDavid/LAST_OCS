2e4d11a2f5eb2278de0f717b47666604a5b08435
Enrico Segre <david.polishook@weizmann.ac.il>
2023-05-30 08:45:38 +0000

    WIP webunit - implementing handles and .Status

--- +obs/@webunit/webunit.m
+++ +obs/@webunit/webunit.m
@@ -18,7 +18,7 @@ classdef webunit < obs.LAST_Handle
 
     % these are filled canonically by the constructor
     properties (GetAccess=public, SetAccess=private)
-        Mount cell     % handle to the mount(s) abstraction object
+        Mount     % handle to the mount(s) abstraction object
         Camera cell    % cell, handles to the camera abstraction objects
         Focuser cell   % cell, handles to the focuser abstraction objects
         PowerSwitch  cell   % handles to IP power switches units
@@ -36,9 +36,12 @@ classdef webunit < obs.LAST_Handle
         MountPowerUnit double =[];% switch controlling mount power
         MountPowerOutput double=[]; % switch output controlling the mount
         MountNumber            = 99;  % Mount number 1..12 - 99=unknown (currently taken from Id)
+        NumLocalTelescopes=2;
+        NumRemoteTelescopes=2;
+        Timezone=2;
     end
 
-    properties(GetAccess=public, SetAccess=private,Description='api')
+    properties(GetAccess=public, SetAccess=private, Description='api')
         Temperature double; % temperature reading from the IPswitch 1wire sensors
         Status struct; % structure holding the status of all components
     end
@@ -46,7 +49,7 @@ classdef webunit < obs.LAST_Handle
 
     methods
         % constructor, destructor
-        function UnitObj=webunit(Locator)
+        function U=webunit(Locator)
             % identify the unit by locator
             if exist('Locator','var') 
                 if isa(Locator,'obs.api.Locator')
@@ -57,22 +60,22 @@ classdef webunit < obs.LAST_Handle
             else
                 id=''; % will cause error below
             end
-            UnitObj.Id=id;
+            U.Id=id;
             % fill initial status of untyped .Connected
-            UnitObj.Connected=false;
+            U.Connected=false;
             % load configuration
-            UnitObj.loadConfig(UnitObj.configFileName('create'))
+            U.loadConfig(U.configFileName('create'))
             % this one is read in as string and converted, because of limitations of
             %  Astropack's yml reader
-%             UnitObj.RemoteTelescopes=eval(UnitObj.RemoteTelescopes);
+%             U.RemoteTelescopes=eval(U.RemoteTelescopes);
 %                         
              % Populate mount, camera, focuser and power switches handles
              % NB: it would be sensible and elegant to construct locators
              %     using the relevant arguments of the constructor, e.g.
              %
-             %  obs.api.Locator('ProjectName',UnitObj.ProjectName,...
-             %                  'NodeId',UnitObj.NodeId,...
-             %                  'MountId',UnitObj.MountId,'EquipType','Mount');
+             %  obs.api.Locator('ProjectName',U.ProjectName,...
+             %                  'NodeId',U.NodeId,...
+             %                  'MountId',U.MountId,'EquipType','Mount');
              %
              % but parsing of the arguments is yet imperfect. (see
              % https://github.com/blumzi/LAST_Api/issues/29). Thus, for the
@@ -82,59 +85,57 @@ classdef webunit < obs.LAST_Handle
              % regenerate (in case passed as string) the full locator of
              % the unit
              L=obs.api.Locator('Location',id);
+             projectnode=sprintf('%s.%d', L.ProjectName, L.NodeId);
              % switches:
-             sw=sprintf('%s.%d.psw%d', L.ProjectName, L.NodeId, L.MountId);
-             UnitObj.PowerSwitch={
+             sw=sprintf('%s.psw%d', projectnode, L.MountId);
+             U.PowerSwitch={
                  obs.api.makeApi('Location',[sw 'e']), ...
                  obs.api.makeApi('Location',[sw 'w'])};
-%             % for now always one mount per unit (or, empty mount when absent)
-%             UnitObj.Mount=
-%             Nlocal=numel(UnitObj.LocalTelescopes);
-%             Nremote=numel(horzcat(UnitObj.RemoteTelescopes{:}));
-%             UnitObj.Camera=cell(1,Nlocal+Nremote);
-%             UnitObj.Focuser=cell(1,Nlocal+Nremote);
-%             
-%             % create camera and focuser objects for local telescopes,
-%             %  as well as listeners for new images
-%             for i=1:Nlocal
-%                 j=UnitObj.LocalTelescopes(i);
-%                 telescope_label=sprintf('%s_%d_%d',UnitObj.Id,1,j);
-%                 UnitObj.Camera{j}=eval([UnitObj.CameraDriver{i} ...
-%                                         '(''' telescope_label ''')']);
-%                 UnitObj.Focuser{j}=eval([UnitObj.FocuserDriver{i} ...
-%                                         '(''' telescope_label ''')']);
-%                 % better listener or addlistener?
-%                 addlistener(UnitObj.Camera{j},'LastImage','PostSet',@UnitObj.treatNewImage);
-%             end
-%             
-%             % create remoteClass objects for remote telescopes
-%             for i=horzcat(UnitObj.RemoteTelescopes{:})
-%                UnitObj.Camera{i}=obs.remoteClass;              
-%                UnitObj.Focuser{i}=obs.remoteClass;
-%             end
+             % for now always one mount per unit (or, empty mount when absent)
+             U.Mount=obs.api.makeApi('Location',...
+                 sprintf('%s.mount%de', projectnode, L.MountId),'ForceRemote',true);
+%    BUG: 'ForceRemote' should be true, see https://github.com/blumzi/LAST_Api/issues/30
+            % create camera and focuser objects for local telescopes
+            U.Camera=cell(1,U.NumLocalTelescopes+U.NumRemoteTelescopes);
+            U.Focuser=cell(1,U.NumLocalTelescopes+U.NumRemoteTelescopes);
+            % sides hardwired for the moment, locator parsing has bugs
+            for j=1:2
+                U.Camera{j}=obs.api.makeApi('Location',...
+                      sprintf('%s.mount%de.camera%d',projectnode,L.MountId,j),...
+                      'ForceRemote',true);
+                U.Camera{j+2}=obs.api.makeApi('Location',...
+                      sprintf('%s.mount%dw.camera%d',projectnode,L.MountId,j+2),...
+                      'ForceRemote',~true);
+                U.Focuser{j}=obs.api.makeApi('Location',...
+                      sprintf('%s.mount%de.focuser%d',projectnode,L.MountId,j),...
+                      'ForceRemote',true);
+                U.Focuser{j+2}=obs.api.makeApi('Location',...
+                      sprintf('%s.mount%dw.focuser%d',projectnode,L.MountId,j+2),...
+                      'ForceRemote',~true);
+            end
             
         end
         
 
-        function delete(UnitObj)
+        function delete(U)
             % delete unit object and related sub objects
 % Be careful. Since the resources for each child are in fact unique (fixed
 %  udp port numbers, fixed switch, focuser, mount), it can very well
 %  happen that the destructor of an old object is called just after
 %  a new one is created, causing hardware to be turned off immediately
 %  after it is turned on, if the specific delete() includes that.
-            for i=1:numel(UnitObj.Slave)
-                delete(UnitObj.Slave{i})
+            if U.Connected
+                U.disconnect;
             end
-            delete(UnitObj.Mount);
-            for i=1:numel(UnitObj.Camera)
-                delete(UnitObj.Camera{i});
+            delete(U.Mount);
+            for i=1:numel(U.Camera)
+                delete(U.Camera{i});
             end
-            for i=1:numel(UnitObj.Focuser)
-                delete(UnitObj.Focuser{i});
+            for i=1:numel(U.Focuser)
+                delete(U.Focuser{i});
             end
-            for i=1:numel(UnitObj.PowerSwitch)
-                delete(UnitObj.PowerSwitch{i});
+            for i=1:numel(U.PowerSwitch)
+                delete(U.PowerSwitch{i});
             end
         end
                         
@@ -142,64 +143,106 @@ classdef webunit < obs.LAST_Handle
     
     % setters/getters
     methods
-        function power=get.MountPower(UnitObj)
+
+        function status=get.Status(U)
+            try
+                if U.Mount.Connected
+                    status.Mount=U.Mount.Status;
+                else
+                    status.Mount='disconnected';
+                end
+            catch
+                status.Mount='unreachable';
+            end
+            %
+            status.Camera=cell(1,numel(U.Camera));
+            for i=1:numel(U.Camera)
+                try
+                    if U.Camera{i}.Connected
+                        status.Camera{i}=U.Camera{i}.CamStatus;
+                    else
+                        status.Camera{i}='disconnected';
+                    end
+                catch
+                    status.Camera{i}='unreachable';
+                end
+            end
+            %
+            status.Focuser=cell(1,numel(U.Focuser));
+            for i=1:numel(U.Focuser)
+                try
+                    if U.Camera{i}.Connected
+                        status.Focuser{i}=U.Focuser{i}.Status;
+                    else
+                        status.Focuser{i}='disconnected';
+                    end
+                catch
+                    status.Focuser{i}='unreachable';
+                end
+            end
+            %
+            status.Switch=cell(1,numel(U.PowerSwitch));
+            for i=1:numel(U.PowerSwitch)
+                try 
+                   if U.PowerSwitch{i}.Connected
+                      status.Switch{i}='connected';
+                   else
+                      status.Switch{i}='disconnected';
+                   end
+                catch
+                    status.Switch{i}='unreachable';
+                end
+            end
+        end
+
+
+        function power=get.MountPower(U)
             try
                 power=...
-                    UnitObj.PowerSwitch{UnitObj.MountPowerUnit}.classCommand('Outputs(%d);',...
-                                                UnitObj.MountPowerOutput);
+                    U.PowerSwitch{U.MountPowerUnit}.Outputs(U.MountPowerOutput);
             catch
                 power=false;
             end
         end
-        
-        function set.MountPower(UnitObj,power)
-            UnitObj.PowerSwitch{UnitObj.MountPowerUnit}.classCommand('Outputs(%d)=%d;',...
-                                UnitObj.MountPowerOutput,power);
+
+
+        function set.MountPower(U,power)
+            U.PowerSwitch{U.MountPowerUnit}.Outputs(U.MountPowerOutput)=power;
         end
-        
-        function Power=get.CameraPower(UnitObj)
-            numcam=numel(UnitObj.Camera);
+
+
+        function Power=get.CameraPower(U)
+            numcam=numel(U.Camera);
             Power=false(1,numcam);
-            Switches=unique(UnitObj.CameraPowerUnit);
+            Switches=unique(U.CameraPowerUnit);
             for i=1:numel(Switches)
-                onThisSwitch=UnitObj.CameraPowerUnit==Switches(i);
+                onThisSwitch=U.CameraPowerUnit==Switches(i);
                 try
                     outputs=...
-                        UnitObj.PowerSwitch{Switches(i)}.classCommand('Outputs;');
+                        U.PowerSwitch{Switches(i)}.Outputs;
                     Power(onThisSwitch)=...
-                         outputs(UnitObj.CameraPowerOutput(onThisSwitch));
+                         outputs(U.CameraPowerOutput(onThisSwitch));
                 catch
                     Power(onThisSwitch)=false;
                 end
             end
         end
         
-        function set.CameraPower(UnitObj,power)
-            numcam=numel(UnitObj.Camera);
+        function set.CameraPower(U,power)
+            numcam=numel(U.Camera);
             for i=1:min(numcam,numel(power))
-                IPswitch=UnitObj.PowerSwitch{UnitObj.CameraPowerUnit(i)};
-                IPoutput=UnitObj.CameraPowerOutput(i);
-                IPswitch.classCommand('OutputN(%d,%d);',IPoutput,power(i));
+                IPswitch=U.PowerSwitch{U.CameraPowerUnit(i)};
+                IPoutput=U.CameraPowerOutput(i);
+                IPswitch.Outputs(IPoutput)=power(i);
             end
         end
         
-        function Result = get.MountNumber(UnitObj)
-            % getter for MountNumber
-            % currently taken from 'Id' property, which is fragile,
-            %  in anticipation of a better handling
-           
-            Result = sscanf(UnitObj.Id,'%d');
-            if isempty(Result)
-                Result = 99;
-            end
-             
-        end
         
-        function T=get.Temperature(UnitObj)
-            N=numel(UnitObj.PowerSwitch);
+        function T=get.Temperature(U)
+            N=numel(U.PowerSwitch);
             T=NaN(1,N);
             for i=1:N
-                T(i)= UnitObj.PowerSwitch{i}.classCommand('Sensors.TemperatureSensors(1)');
+                T(i)= U.PowerSwitch{i}.Sensors.TemperatureSensors(1)';
             end
         end
     end
