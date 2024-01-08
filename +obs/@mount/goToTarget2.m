4f8853acce1a4f975c594687f7425bacddd05110
EranOfek <ofek.eran@gmail.com>
2024-01-08 11:54:47 +0200

    goToTarget2

--- +obs/@mount/goToTarget2.m
+++ +obs/@mount/goToTarget2.m
@@ -15,7 +15,7 @@ function [Flag,OutRA,OutDec,Aux]=goToTarget2(MountObj, RA, Dec, Shift, ApplyDist
     %          - A logical indicating if to apply the distortion
     %            corrections. Default is true.
     %
-    % Output : - A logical flag indicating if sucessful.
+    % Output : - A logical flag indicatirng if sucessful.
     %          See code for additional output arguments.
     % Author : Eran Ofek (Jan 2024)
     % Example: M.goToTarget2(150, +20, '1')
@@ -58,139 +58,97 @@ function [Flag,OutRA,OutDec,Aux]=goToTarget2(MountObj, RA, Dec, Shift, ApplyDist
     Aux.HA_AppDist  = NaN;
     Aux.Dec_AppDist = NaN;
 
+    % FFU: read from congiguration
+    if ischar(Shift)
+        switch Shift
+            case '1'
+                Shift = -[+1.65 +1.1];
+            case '2'
+                Shift = -[+1.65 -1.1];
+            case '3'
+                Shift = -[-1.65 -1.1];
+            case '4'
+                Shift = -[-1.65 +1.1];
+            otherwise
+                error('Unknwon Shift option');
+        end
+    end
 
-    switch lower(MountObj.Status)
-        case 'park'
-            MountObj.LogFile.write('Error: Attempt to slew telescope while parking');
-            Flag = false;
-            %error('Can not slew telescope while parking');
-        otherwise
-            % Convert input into RA/Dec [input deg, output deg]
-            GeoPos = [MountObj.ObsLon./RAD, MountObj.ObsLat./RAD, MountObj.ObsHeight];   % [rad rad m]
 
-            % FFU: considering reading meterorological data...
-            MetData.Wave = 5000; % A
-            MetData.Temp = 15;   % C
-            MetData.P    = 760;  % Hg
-            MetData.Pw   = 8;    % Hg
-            [OutRA, OutDec, Alt, Refraction, Aux] = celestial.convert.apparentStarPos(RA, Dec, JD,...
-                                                               'InUnits','deg',...
-                                                               'Epoch',2000,...
-                                                               'OutUnits','deg',...
-                                                               'OutEquinox',[],...
-                                                               'OutEquinoxUnits','JD',...
-                                                               'OutMean',false,...
-                                                               'PM_RA',0,...
-                                                               'PM_Dec',0,...
-                                                               'PM_Plx',1e-2,...
-                                                               'RV',0,...
-                                                               'INPOP',MountObj.INPOP,...
-                                                               'GeoPos',GeoPos,...
-                                                               'TypeLST','m',...
-                                                               'ApplyAberration',true,...
-                                                               'ApplyRefraction',true,...
-                                                               'Wave',MetData.Wave,...
-                                                               'Temp',MetData.Temp,...
-                                                               'Pressure',MetData.P,...
-                                                               'Pw',MetData.Pw);
+    if max(abs(Shift))>2
+        Flag = false;
+        error('Shift of more than 2 deg is not allowed');
+    else
+        
+        switch lower(MountObj.Status)
+            case 'park'
+                MountObj.LogFile.write('Error: Attempt to slew telescope while parking');
+                Flag = false;
+                %error('Can not slew telescope while parking');
+            otherwise
+                % Convert input into RA/Dec [input deg, output deg]
+                GeoPos = [MountObj.ObsLon./RAD, MountObj.ObsLat./RAD, MountObj.ObsHeight];   % [rad rad m]
+    
+    
+                % treat Shift
+    
+    
+                % FFU: considering reading meterorological data...
+                MetData.Wave = 5000; % A
+                MetData.Temp = 15;   % C
+                MetData.P    = 760;  % Hg
+                MetData.Pw   = 8;    % Hg
+                [OutRA, OutDec, Alt, Refraction, Aux] = celestial.convert.j2000_toApparent(RA, Dec, JD,...
+                                                                   'InUnits','deg',...
+                                                                   'Epoch',2000,...
+                                                                   'OutUnits','deg',...
+                                                                   'OutEquinox',[],...
+                                                                   'OutEquinoxUnits','JD',...
+                                                                   'OutMean',false,...
+                                                                   'PM_RA',0,...
+                                                                   'PM_Dec',0,...
+                                                                   'PM_Plx',1e-2,...
+                                                                   'RV',0,...
+                                                                   'INPOP',MountObj.INPOP,...
+                                                                   'GeoPos',GeoPos,...
+                                                                   'TypeLST','m',...
+                                                                   'ApplyAberration',true,...
+                                                                   'ApplyRefraction',true,...
+                                                                   'Wave',MetData.Wave,...
+                                                                   'Temp',MetData.Temp,...
+                                                                   'Pressure',MetData.P,...
+                                                                   'Pw',MetData.Pw,...
+                                                                   'ShiftRA',Shift(1),...
+                                                                   'ShiftDec',Shift(2),...
+                                                                   'ApplyDistortion',ApplyDist,...
+                                                                   'InterpHA',MountObj.PointingModel.InterpHA,...
+                                                                   'InterpDec',MountObj.PointingModel.InterpDec);
 
             
-
-            if Alt<MinAlt
-                MountObj.LogFile.write(sprintf('Error: Target requested altitude (%f) is below limit (%f)',Alt,MinAlt));
-                Flag = false;
-                
-            else
-                % Aux contains:
-                % .RA_J2000
-                % .Dec_J2000
-                % .RA_App
-                % .Dec_App
-    
-                % add .HA_App
-                Aux.HA_App = celestial.convert.convert_ha(Aux.RA_App, JD, 'InUnits','deg', 'OutUnits','deg',...
-                                                             'Long',GeoPos(1),...
-                                                             'LongUnits','rad',...
-                                                             'TypeLST','a',...
-                                                             'OutRange','pi');
-
-
-                % apply distortions to apparent coordinates
-                if ApplyDist
-                    % InterpHA and InterpDec contains the interpolating
-                    % function to calculate the shift to add to the apparent HA and Dec
-                    % in order to get the mount coordinates [deg]
-    
-                    Aux.HA_AppDist  = Aux.HA_App  + MountObj.PointingModel.InterpHA(Aux.HA_App, Aux.Dec_App);
-                    Aux.Dec_AppDist = Aux.Dec_App + MountObj.PointingModel.InterpDec(Aux.HA_App, Aux.Dec_App);
-
-                    % convert HA to RA
-                    Aux.RA_AppDist = celestial.convert.convert_ha(Aux.HA_AppDist, JD, 'InUnits','deg', 'OutUnits','deg',...
-                                                             'Long',GeoPos(1),...
-                                                             'LongUnits','rad',...
-                                                             'TypeLST','a',...
-                                                             'OutRange','pi');
-                    OutRA  = Aux.RA_AppDist;
-                    OutDec = Aux.Dec_AppDist;
+                % verification:
+                if Alt<MinAlt
+                    MountObj.LogFile.write(sprintf('Error: Target requested altitude (%f) is below limit (%f)',Alt,MinAlt));
+                    Flag = false;
                 end
-    
-                % verify HA is in allowed range:
                 if abs(Aux.HA_App)>HALimit
                     MountObj.LogFile.write(sprintf('Error: Requested HA (%f) is out of allowd range',Aux.HA_App));
                     Flag = false;
-                else
-
-                    if ischar(Shift)
-                        switch Shift
-                            case '1'
-                                Shift = -[+1.65 +1.1];
-                            case '2'
-                                Shift = -[+1.65 -1.1];
-                            case '3'
-                                Shift = -[-1.65 -1.1];
-                            case '4'
-                                Shift = -[-1.65 +1.1];
-                            otherwise
-                                error('Unknwon Shift option');
-                        end
-                    end
-                    if max(abs(Shift))>2
-                        Flag = false;
-                        error('Shift of more than 2 deg is not allowed');
-                    else
-                        ShiftRA  = Shift(1)./cosd(OutDec);
-                        ShiftDec = Shift(2);
-                        OutRA    = OutRA  + ShiftRA;
-                        OutDec   = OutDec + ShiftDec;
-    
-                        Aux.RA_J2000    = Aux.RA_J2000 + ShiftRA;
-                        Aux.Dec_J2000   = Aux.Dec_J2000 + ShiftDec;
-                        Aux.RA_App      = Aux.RA_App + ShiftRA;
-                        Aux.HA_App      = Aux.HA_App + ShiftRA;
-                        Aux.Dec_App     = Aux.Dec_App + ShiftDec;
-                        Aux.RA_AppDist  = Aux.RA_AppDist + ShiftRA;
-                        Aux.HA_AppDist  = Aux.HA_AppDist + ShiftRA;
-                        Aux.Dec_AppDist = Aux.Dec_AppDist + ShiftDec;
-    
-                        Flag = true;
-        
-                        % go to target
-                        MountObj.goTo(OutRA, OutDec, 'eq');
-                    end
                 end
-            end
 
-
+        end
+        
     end
 
     % write coordinates to MountObj
-    MountObj.RA_J2000   = Aux.RA_J2000;
-    MountObj.Dec_J2000  = Aux.Dec_J2000;
-    MountObj.RA_App     = Aux.RA_App;
-    MountObj.HA_App     = Aux.HA_App;
-    MountObj.Dec_App    = Aux.Dec_App;
-    MountObj.RA_AppDist = Aux.RA_AppDist;
-    MountObj.HA_AppDist = Aux.HA_AppDist;
-    MountObj.Dec_AppDist= Aux.Dec_AppDist;
+    % MountObj.RA_J2000   = Aux.RA_J2000;     % M_JRA
+    % MountObj.Dec_J2000  = Aux.Dec_J2000;    % M_JDEC
+    % MountObj.RA_App     = Aux.RA_App;       % M_ARA
+    % MountObj.HA_App     = Aux.HA_App;       % M_AHA
+    % MountObj.Dec_App    = Aux.Dec_App;      % M_ADEC
+    % MountObj.RA_AppDist = Aux.RA_AppDist;   % M_ADRA
+    % MountObj.HA_AppDist = Aux.HA_AppDist;   % M_ADHA
+    % MountObj.Dec_AppDist= Aux.Dec_AppDist;  % M_ADDEC
+    %                                         % RA = M_JRA + CamShiftRA./cosd(M_JDEC)
+    %                                         % DEC = M_JDEC + CameraShiftDec 
 
 end
