2519cdceb499a6587472882487ecc010ca762fea
Enrico Segre <enrico.segre@weizmann.ac.il>
2024-01-30 13:05:05 +0000

    reverting the last 2 commits, to restore correct version of pointingModel2.m
    Revert "renamed pointingModel2 to pointingModel and moved the old version (that uses goTo) to crap"
    
    This reverts commit 8093b49d5804d545af6d51611c5b5cd586bd4a70.

--- +obs/+util/+align/pointingModel.m
+++ +obs/+util/+align/pointingModel.m
@@ -1,90 +1,79 @@
 function pointingModel(Unit, Args)
-    % record pointing model data using goToTarget2
-    % Input  : 'Unit' - the unit object
-    %          'Nha' - number of HA points used when tiling the sky
-    %          'Ndec' - number of Dec points used when tiling the sky
-    %          'MinAlt' - only observe positions above this altitude [deg]
-    %          'ExpTime' - exposure time [s]
-    %          'ObsCoo' - observatory Long Lat [deg deg]. Default [35, 30].
-    %          'Object' - string that will appear in file name. Default
-    %          is "PointModel"
-    %          'ApplyDistortion' - logical use existing pointing model or
-    %          not. Should be false to record a new pointing model. Default 
-    %          is false.
-    %
-    % Output : - None
-    % Author : Nora Strotjohann (Jan 2024)
-    % Example: obs.util.align.pointingModel(Unit)
+    % Collect images for a simple pointing model
+    % Example: obs.util.align.pointingModel
    
     arguments
         Unit
-        Args.Nha      = 20; %30
-        Args.Ndec     = 10; %15
+        Args.Nha      = 30;
+        Args.Ndec     = 15;
         Args.MinAlt   = 25; % [deg]
         Args.ExpTime  = 1;     % if empty - only move without exposing
-        Args.ObsCoo   = [35, 30];
-        Args.Object   = 'PointingModel';
-        Args.ApplyDistortion = false;
+        Args.ObsCoo   = [35, 30]
+        Args.Tracking logical   = true;
+        Args.ClearFaults logical = false;
     end
     
     RAD = 180./pi;
     
-    % timeout for cameras used in readyToExpose
-    if isempty(Args.ExpTime)
-        Timeout = 60;
-    else
-        Timeout = Args.ExpTime+60;
-    end
-    
     % make grid
+        
     [TileList,~] = celestial.grid.tile_the_sky(Args.Nha, Args.Ndec);
     HADec = TileList(:,1:2);
-
+        
+    
     [~, Alt] = celestial.coo.hadec2azalt(HADec(:,1), HADec(:,2), ...
         Args.ObsCoo(2)./RAD);
-    
     
     % convert everything to degrees
     Alt = Alt*RAD;
     HADec = HADec*RAD;
     
-    HADec = HADec(Alt>Args.MinAlt,:);
-    Alt = Alt(Alt>Args.MinAlt);
-    
-    Ntarget = length(HADec(:,1));
+    Flag = Alt>(Args.MinAlt);
+    HADec = HADec(Flag,:);
+    Ntarget = sum(Flag);
     disp('Will observe '+string(Ntarget)+' fields.')
     
     for Itarget=1:1:Ntarget
+        fprintf('Observe field %d out of %d - HA=%f, Dec=%f\n',Itarget,Ntarget,HADec(Itarget,1), HADec(Itarget,2));
         if exist('~/abort','file')>0
             delete('~/abort');
             error('user abort file found');
         end
-        
-        RA = Unit.Mount.LST - HADec(Itarget,1);
-        if RA<0
-            RA = RA+360;
-        elseif RA>360
-            RA = RA-360;
-        end
-        fprintf('\nObserve field %d out of %d - RA=%.3f, HA=%.3f, Dec=%.3f, Alt=%.3f\n', ...
-            Itarget,Ntarget,RA, HADec(Itarget,1), HADec(Itarget,2), Alt(Itarget));
-
-        Unit.Mount.goToTarget2(RA, HADec(Itarget,2), [0, 0],Args.ApplyDistortion);
-        Unit.Mount.waitFinish;
-
+        if HADec(Itarget,2)>-50
             
-        fprintf('Actual pointing: HA=%.3f, Dec=%.3f\n',Unit.Mount.HA, Unit.Mount.Dec);
+            if Args.ClearFaults
+                Unit.Mount.clearFaults;
+            end
+            %%% TODO call this instead to include all corrections except
+            %%% for the pointing model itself
+            % Unit.Mount.goTo(HADec(Itarget,1), HADec(Itarget,2), 'ha', ...
+            % 	'DistFunHA',[],'DistFunDec',[]);
+            
+            Unit.Mount.goTo(HADec(Itarget,1), HADec(Itarget,2), 'ha');
+            Unit.Mount.waitFinish;
+            if Args.Tracking
+                Unit.Mount.track
+            end
+            pause(3);
+            
+            fprintf('Actual pointing: HA=%f, Dec=%f\n',Unit.Mount.HA, Unit.Mount.Dec);
 
-        if ~isempty(Args.ExpTime)
-            Unit.takeExposure([],Args.ExpTime,1,'Object',Args.Object);
-            fprintf('Wait for exposure to finish\n');
-        end
+            if ~isempty(Args.ExpTime)
+                fprintf('call takeExposure\n');
+                Unit.takeExposure([],Args.ExpTime,1,'Object','PointingModel');
+                fprintf('Wait for exposure to finish\n');
+            end
         
-        pause(Args.ExpTime+3);
-
-        if ~Unit.readyToExpose('Wait',true, 'Timeout',Timeout)
-            Unit.reportError('cameras not ready after timeout - abort')
-            break;
+            pause(Args.ExpTime+6);
+            if isempty(Args.ExpTime)
+                Timeout = 60;
+            else
+                Timeout = Args.ExpTime+60;
+            end
+            if ~Unit.readyToExpose('Wait',true, 'Timeout',Timeout)
+                disp('cameras not ready after timeout - abort')
+                break;
+            end
         end
     end
         
---
+++ +obs/+util/+align/pointingModel2.m
@@ -0,0 +1,91 @@
+function pointingModel2(Unit, Args)
+    % record pointing model data using goToTarget2
+    % Input  : 'Unit' - the unit object
+    %          'Nha' - number of HA points used when tiling the sky
+    %          'Ndec' - number of Dec points used when tiling the sky
+    %          'MinAlt' - only observe positions above this altitude [deg]
+    %          'ExpTime' - exposure time [s]
+    %          'ObsCoo' - observatory Long Lat [deg deg]. Default [35, 30].
+    %          'Object' - string that will appear in file name. Default
+    %          is "PointModel"
+    %          'ApplyDistortion' - logical use existing pointing model or
+    %          not. Should be false to record a new pointing model. Default 
+    %          is false.
+    %
+    % Output : - None
+    % Author : Nora Strotjohann (Jan 2024)
+    % Example: obs.util.align.pointingModel2(Unit)
+   
+    arguments
+        Unit
+        Args.Nha      = 20; %30
+        Args.Ndec     = 10; %15
+        Args.MinAlt   = 25; % [deg]
+        Args.ExpTime  = 1;     % if empty - only move without exposing
+        Args.ObsCoo   = [35, 30];
+        Args.Object   = 'PointingModel';
+        Args.ApplyDistortion = false;
+    end
+    
+    RAD = 180./pi;
+    
+    % timeout for cameras used in readyToExpose
+    if isempty(Args.ExpTime)
+        Timeout = 60;
+    else
+        Timeout = Args.ExpTime+60;
+    end
+    
+    % make grid
+    [TileList,~] = celestial.grid.tile_the_sky(Args.Nha, Args.Ndec);
+    HADec = TileList(:,1:2);
+
+    [~, Alt] = celestial.coo.hadec2azalt(HADec(:,1), HADec(:,2), ...
+        Args.ObsCoo(2)./RAD);
+    
+    
+    % convert everything to degrees
+    Alt = Alt*RAD;
+    HADec = HADec*RAD;
+    
+    HADec = HADec(Alt>Args.MinAlt,:);
+    Alt = Alt(Alt>Args.MinAlt);
+    
+    Ntarget = length(HADec(:,1));
+    disp('Will observe '+string(Ntarget)+' fields.')
+    
+    for Itarget=1:1:Ntarget
+        if exist('~/abort','file')>0
+            delete('~/abort');
+            error('user abort file found');
+        end
+        
+        RA = Unit.Mount.LST - HADec(Itarget,1);
+        if RA<0
+            RA = RA+360;
+        elseif RA>360
+            RA = RA-360;
+        end
+        fprintf('\nObserve field %d out of %d - RA=%.3f, HA=%.3f, Dec=%.3f, Alt=%.3f\n', ...
+            Itarget,Ntarget,RA, HADec(Itarget,1), HADec(Itarget,2), Alt(Itarget));
+
+        Unit.Mount.goToTarget2(RA, HADec(Itarget,2), [0, 0],Args.ApplyDistortion);
+        Unit.Mount.waitFinish;
+
+            
+        fprintf('Actual pointing: HA=%.3f, Dec=%.3f\n',Unit.Mount.HA, Unit.Mount.Dec);
+
+        if ~isempty(Args.ExpTime)
+            Unit.takeExposure([],Args.ExpTime,1,'Object',Args.Object);
+            fprintf('Wait for exposure to finish\n');
+        end
+        
+        pause(Args.ExpTime+3);
+
+        if ~Unit.readyToExpose('Wait',true, 'Timeout',Timeout)
+            Unit.reportError('cameras not ready after timeout - abort')
+            break;
+        end
+    end
+        
+end
