0a86ec583647ec2addc208ae35aba55566441fa9
Enrico Segre <>
2024-04-15 10:30:21 +0000

    more effective lines in testing/messengerPileup

--- testing/messengerPileup.m
+++ testing/messengerPileup.m
@@ -7,3 +7,15 @@ S.query('a',1)
 for i=1:10
     S.query('a',1)
 end
+
+
+S.send('MasterResponder.Verbose=2')
+S.send('MasterMessenger.Verbose=2')
+S.send('i=0')
+for i=1:100;S.send('pause(1);i=i+1');end
+
+S.send('i=0')
+for i=1:100;S.sendCallback('pause(1);i=i+1');end
+
+S.query('MasterMessenger.StreamResource.BytesAvailable')
+S.query('MasterResponder.StreamResource.BytesAvailable')
